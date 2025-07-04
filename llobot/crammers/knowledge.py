a4bd301fd9e37de256b043223c3e3eaeb8dde078
Robert Važan <robert.vazan@tutanota.com>
2025-07-04 11:06:38 +0000

    Removed trimming crammer

--- llobot/crammers/knowledge.py
+++ llobot/crammers/knowledge.py
@@ -25,16 +25,14 @@ class KnowledgeCrammer:
         return llobot.contexts.empty()
 
 @lru_cache
-def trimming(*,
+def priority(*,
     # Eager trimmer is applied to all documents regardless of whether that's necessary to save space.
-    eager_trimmer: Trimmer = llobot.trimmers.eager(),
-    # Regular trimmer is used only when we need to save space. It is terminated automatically.
-    trimmer: Trimmer = llobot.trimmers.none(),
+    trimmer: Trimmer = llobot.trimmers.eager(),
     scorer: KnowledgeScorer = llobot.scorers.knowledge.standard(),
     formatter: KnowledgeFormatter = llobot.formatters.knowledge.standard(),
     ranker: KnowledgeRanker = llobot.knowledge.rankers.standard(),
 ) -> KnowledgeCrammer:
-    class TrimmingKnowledgeCrammer(KnowledgeCrammer):
+    class PriorityKnowledgeCrammer(KnowledgeCrammer):
         def cram(self, knowledge: Knowledge, budget: int, scores: KnowledgeScores | None = None, context: Context = llobot.contexts.empty()) -> Context:
             if budget <= 0 or not knowledge or (scores is not None and not scores):
                 return llobot.contexts.empty()
@@ -45,49 +43,7 @@ def trimming(*,
             # Remove documents with zero score. Scorer can be thus used to exclude irrelevant documents.
             # To make that work though, rescoring must not distribute score around.
             knowledge &= scores
-            knowledge = knowledge.transform(eager_trimmer.trim_fully)
-            terminated_trimmer = trimmer.terminate()
-            prior_knowledge = context.knowledge
-            while True:
-                # Remove documents that have exact copies in context.
-                knowledge -= llobot.knowledge.subsets.create(lambda path, content: path in prior_knowledge and prior_knowledge[path] == content)
-                formatted = formatter(knowledge, ranking)
-                if formatted.cost <= budget:
-                    return formatted
-                # Measure density using formatted documents, so that formatting overhead counts into the weight of the document.
-                # Compute square root to invest available budget evenly to both document diversity and document length (which minimizes per-document overhead).
-                # Document length transform should be configurable via crammer parameters or we should support arbitrary scorer (augmented with formatter overhead).
-                # Scale by ratio of actual content to formatter output length to discourage inclusion of tiny files.
-                cost = formatted.knowledge_cost
-                density = scores / cost.transform(math.sqrt) * (llobot.scores.knowledge.length(knowledge) / cost)
-                density_ranking = list(llobot.knowledge.rankings.descending(density))
-                worst = set(density_ranking[:len(density_ranking)//10+1])
-                trimmed = Knowledge({path: (terminated_trimmer(path, content) if path in worst else content) for path, content in knowledge})
-                if trimmed.cost >= knowledge.cost:
-                    raise RuntimeError('Trimmer failed to reduce size of the knowledge.')
-                knowledge = trimmed
-    return TrimmingKnowledgeCrammer()
-
-@lru_cache
-def whole(*,
-    # Eager trimmer is applied to all documents regardless of whether that's necessary to save space.
-    eager_trimmer: Trimmer = llobot.trimmers.eager(),
-    scorer: KnowledgeScorer = llobot.scorers.knowledge.standard(),
-    formatter: KnowledgeFormatter = llobot.formatters.knowledge.standard(),
-    ranker: KnowledgeRanker = llobot.knowledge.rankers.standard(),
-) -> KnowledgeCrammer:
-    class WholeKnowledgeCrammer(KnowledgeCrammer):
-        def cram(self, knowledge: Knowledge, budget: int, scores: KnowledgeScores | None = None, context: Context = llobot.contexts.empty()) -> Context:
-            if budget <= 0 or not knowledge or (scores is not None and not scores):
-                return llobot.contexts.empty()
-            knowledge = knowledge.strip()
-            ranking = ranker(knowledge)
-            knowledge &= ranking
-            scores = scorer.rescore(knowledge, scores) if scores is not None else scorer(knowledge)
-            # Remove documents with zero score. Scorer can be thus used to exclude irrelevant documents.
-            # To make that work though, rescoring must not distribute score around.
-            knowledge &= scores
-            knowledge = knowledge.transform(eager_trimmer.trim_fully)
+            knowledge = knowledge.transform(trimmer.trim_fully)
             # Premultiply with document lengths. Both scores and lengths will get a denominator in the loop.
             scores *= llobot.scores.knowledge.length(knowledge)
             prior_knowledge = context.knowledge
@@ -113,26 +69,25 @@ def whole(*,
                     length -= costs[path]
                     removed.append(path)
                 knowledge -= KnowledgeIndex(removed)
-    return WholeKnowledgeCrammer()
+    return PriorityKnowledgeCrammer()
 
 @cache
 def standard() -> KnowledgeCrammer:
-    return whole()
+    return priority()
 
 @cache
 def retrieval() -> KnowledgeCrammer:
-    return whole(scorer=llobot.scorers.knowledge.constant())
+    return priority(scorer=llobot.scorers.knowledge.constant())
 
 # Intended to be used for files that are already in the context but they have changed since.
 @cache
 def updates() -> KnowledgeCrammer:
     # We would ideally want to score relevance, but that's currently not possible, because score parameter is used for filtering.
-    return whole(formatter=llobot.formatters.knowledge.updates(), scorer=llobot.scorers.knowledge.constant())
+    return priority(formatter=llobot.formatters.knowledge.updates(), scorer=llobot.scorers.knowledge.constant())
 
 __all__ = [
     'KnowledgeCrammer',
-    'trimming',
-    'whole',
+    'priority',
     'standard',
     'retrieval',
     'updates',
