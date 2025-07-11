d0073eead3549fbb90845a0d203597d39218b12b
Robert Važan <robert.vazan@tutanota.com>
2025-07-11 17:29:08 +0000

    Default to larger share of examples

--- llobot/roles/editors.py
+++ llobot/roles/editors.py
@@ -44,8 +44,9 @@ def standard(*,
     update_crammer: KnowledgeCrammer = llobot.crammers.knowledge.updates(),
     retrieval_crammer: KnowledgeCrammer = llobot.crammers.knowledge.retrieval(),
     knowledge_share: float = 1.0,
-    # This share is directly occupied by examples. Examples however consume space also indirectly by forcing stuffing of updates.
-    example_share: float = 0.2,
+    # This share is directly occupied by examples.
+    # Examples however consume space also indirectly by forcing stuffing of updates.
+    example_share: float = 0.5,
     retrieval_share: float = 1.0,
 ) -> Role:
     if isinstance(relevance_scorer, KnowledgeSubset):
