796b1fd5f565a7044a3c17971803395fc5d7841e
Robert Važan <robert.vazan@tutanota.com>
2025-08-09 12:58:52 +0000

    Removed java links

--- llobot/links/java.py
+++
@@ -1,9 +0,0 @@
-from llobot.links import Link
-import llobot.links
-
-# This is only intended to work for top-level types. We are assuming that type names are globally unique.
-def type_name(name: str) -> Link:
-    return llobot.links.filename(f'{name}.java')
-
-__all__ = ['type_name']
-
--- llobot/scrapers/java.py
+++ llobot/scrapers/java.py
@@ -6,7 +6,7 @@ from llobot.knowledge import Knowledge
 from llobot.knowledge.graphs import KnowledgeGraph, KnowledgeGraphBuilder
 from llobot.scrapers import GraphScraper
 import llobot.scrapers
-import llobot.links.java
+import llobot.scrapers.index
 
 @cache
 def pascal_case() -> GraphScraper:
@@ -16,7 +16,7 @@ def pascal_case() -> GraphScraper:
     pattern = re.compile(r'\b[A-Z][A-Za-z0-9]*\b')
     def scrape(knowledge: Knowledge) -> KnowledgeGraph:
         builder = KnowledgeGraphBuilder()
-        indexes = {}
+        index = llobot.scrapers.index.create(knowledge)
         for path, content in knowledge:
             if path.suffix == '.java':
                 content = comment_re.sub(' ', content)
@@ -26,8 +26,8 @@ def pascal_case() -> GraphScraper:
                 for name in names:
                     # Require at least one lowercase letter to avoid matching enums and constants.
                     if not name.isupper():
-                        link = llobot.links.java.type_name(name)
-                        for target in link.resolve_indexed(knowledge, indexes):
+                        target = index.lookup(path, Path(f'{name}.java'))
+                        if target:
                             builder.add(path, target)
         return builder.build()
     return llobot.scrapers.create(scrape)
